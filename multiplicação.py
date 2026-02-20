b2aeb453020c8309a24b0921181e400114ea3fa6
JoaoMoura1079 <jo.moura1079@gmail.com>
2026-02-19 22:26:34 -0300

    Add files via upload

---
+++ multiplicação.py
@@ -0,0 +1,15 @@
+#multiplicação
+
+print("#################################################################")
+print("                          multiplicação                          ")
+print("#################################################################")
+
+a = input ("Entre com o primeiro número e tecle enter: ")
+b = input ("Entre com o segundo número e tecle enter: ")
+
+a = int(a)
+b = int(b)
+
+print (a * b)
+
+input()
---
+++ raíz_quadrada.py
@@ -0,0 +1,15 @@
+#raíz quadrada
+
+print("#################################################################")
+print("                          raíz quadrada                          ")
+print("#################################################################")
+
+import math
+
+a = input ("Entre com o número que quer a raíz quadrada e tecle enter: ")
+
+a = int(a)
+
+print (math.sqrt(a))
+
+input()
